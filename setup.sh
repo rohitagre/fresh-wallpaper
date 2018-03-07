0f3f5f3dd799d790bef01175a0e5cdb7ce4862b7
Rohit <jammer99@boomboxninjas.in>
2018-03-07 16:40:18 +0530

    change wallpaper at 11.35

--- setup.sh
+++ setup.sh
@@ -9,8 +9,7 @@ fi
 echo "This Script will create a new directory 'unsplash-wallpapers' \n under the Pictures Folder for the current user (~/Pictures/)"
 
 read -r -p "Do you wish to Continue? [y/N] " response
-if [[ $response =~ ^[Yy]$ ]];
-then
+if [[ $response =~ ^[Yy]$ ]]; then
 mkdir -p ~/Pictures/unsplash-wallpapers
 echo "\nSelect The category (search term) from which you want images to be downloaded? [ex. adventure, nature, business, love etc]"
 
@@ -22,14 +21,13 @@ sed -i "" '/fresh-wallpaper/d' 'mycron.txt'
 
 read -r -p "Leave blank to fetch daily picture : " cat
 
-if [[ -z "$cat" ]];
-then
+if [[ -z "$cat" ]]; then
 echo "30 11 * * * curl -L --compressed "https://source.unsplash.com/1920x1080/daily" -o ~/Pictures/unsplash-wallpapers/\`date +"\\%d\\%m\\%Y\\%H"\`.jpg  >/dev/null 2>&1 #fresh-wallpaper" >> mycron.txt
 else
-echo "35 11 * * * curl -L --compressed "https://source.unsplash.com/1920x1080/?$cat" -o ~/Pictures/unsplash-wallpapers/\`date +"\\%d\\%m\\%Y\\%H"\`.jpg  >/dev/null 2>&1 #fresh-wallpaper" >> mycron.txt
+echo "30 11 * * * curl -L --compressed "https://source.unsplash.com/1920x1080/?$cat" -o ~/Pictures/unsplash-wallpapers/\`date +"\\%d\\%m\\%Y\\%H"\`.jpg  >/dev/null 2>&1 #fresh-wallpaper" >> mycron.txt
 fi
 
-echo "* * * * * osascript -e 'tell application \"System Events\" to tell every desktop to set picture to \"~/Pictures/unsplash-wallpapers/\`date +'\\%d\\%m\\%Y\\%H'\`.jpg\"' #fresh-wallpaper" >> mycron.txt
+echo "35 11 * * * osascript -e 'tell application \"System Events\" to tell every desktop to set picture to \"~/Pictures/unsplash-wallpapers/\`date +'\\%d\\%m\\%Y\\%H'\`.jpg\"' #fresh-wallpaper" >> mycron.txt
 
 #install new cron file
 
